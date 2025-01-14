ed99edbaa95bb622568df4d5d46fdfdd7bf4e7a9
AndradeTC86 <andrade-tc@uol.com.br>
2025-01-14 20:32:06 -0300

    Refatorando codigo

--- page_objects/calculadora.py
+++ page_objects/calculadora.py
@@ -6,79 +6,71 @@ from selenium.webdriver.common.by import By
 class Calculadora:
     def __init__(self, driver):
         self.driver = driver
-        self.num0 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_0')))
-        self.num1 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_1')))
-        self.num2 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_2')))
-        self.num3 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_3')))
-        self.num4 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_4')))
-        self.num5 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_5')))
-        self.num6 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_6')))
-        self.num7 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_7')))
-        self.num8 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_8')))
-        self.num9 = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/digit_9')))
-        self.ponto = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/dec_point')))
-        self.add = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/op_add')))
-        self.sub = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/op_sub')))
-        self.mul = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/op_mul')))
-        self.div = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/op_div')))
-        self.eq = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/eq')))
-        self.delete = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/del')))
-        self.formula = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/formula')))
-        self.result = WebDriverWait(self.driver.instance, 1) \
-            .until(EC.presence_of_element_located((By.ID, 'com.android.calculator2:id/result')))
+        self.elements = {}
+        self._initialize_elements()
+
+    def _initialize_elements(self):
+        element_ids = {
+            'num0': 'com.android.calculator2:id/digit_0',
+            'num1': 'com.android.calculator2:id/digit_1',
+            'num2': 'com.android.calculator2:id/digit_2',
+            'num3': 'com.android.calculator2:id/digit_3',
+            'num4': 'com.android.calculator2:id/digit_4',
+            'num5': 'com.android.calculator2:id/digit_5',
+            'num6': 'com.android.calculator2:id/digit_6',
+            'num7': 'com.android.calculator2:id/digit_7',
+            'num8': 'com.android.calculator2:id/digit_8',
+            'num9': 'com.android.calculator2:id/digit_9',
+            'ponto': 'com.android.calculator2:id/dec_point',
+            'add': 'com.android.calculator2:id/op_add',
+            'sub': 'com.android.calculator2:id/op_sub',
+            'mul': 'com.android.calculator2:id/op_mul',
+            'div': 'com.android.calculator2:id/op_div',
+            'eq': 'com.android.calculator2:id/eq',
+            'delete': 'com.android.calculator2:id/del',
+            'formula': 'com.android.calculator2:id/formula',
+            'result': 'com.android.calculator2:id/result'
+        }
+        for key, value in element_ids.items():
+            try:
+                self.elements[key] = WebDriverWait(self.driver.instance, 1).until(
+                    EC.presence_of_element_located((By.ID, value))
+                )
+            except Exception as e:
+                print(f"Error initializing element {key}: {e}")
 
     def clicarnumero(self, num):
         select = str(num)
-        self.driver.instance.find_element(By.ID, 'com.android.calculator2:id/digit_' + select).click()
+        self.elements[f'num{select}'].click()
 
     def somar(self, num1, num2):
         self.clicarnumero(num1)
-        self.add.click()
+        self.elements['add'].click()
         self.clicarnumero(num2)
-        formula = self.formula.text
-        result = int(self.result.text)
+        formula = self.elements['formula'].text
+        result = int(self.elements['result'].text)
         return formula, result
 
     def subtrair(self, num1, num2):
         self.clicarnumero(num1)
-        self.sub.click()
+        self.elements['sub'].click()
         self.clicarnumero(num2)
-        formula = self.formula.text
-        result = int(self.result.text)
+        formula = self.elements['formula'].text
+        result = int(self.elements['result'].text)
         return formula, result
 
     def dividir(self, num1, num2):
         self.clicarnumero(num1)
-        self.div.click()
+        self.elements['div'].click()
         self.clicarnumero(num2)
-        formula = self.formula.text
-        result = int(self.result.text)
+        formula = self.elements['formula'].text
+        result = int(self.elements['result'].text)
         return formula, result
 
     def multiplicar(self, num1, num2):
         self.clicarnumero(num1)
-        self.mul.click()
+        self.elements['mul'].click()
         self.clicarnumero(num2)
-        formula = self.formula.text
-        result = int(self.result.text)
+        formula = self.elements['formula'].text
+        result = int(self.elements['result'].text)
         return formula, result
-
-
